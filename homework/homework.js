63d741f74c0aa04f98c4b35e85a84a3b4d71d344
saminjae <smj123432@gmail.com>
2026-01-11 18:00:32 +0900

    과제: 함수  작성

--- homework/homework.js
+++ homework/homework.js
@@ -25,3 +25,76 @@ let userRankPoint = 85.5;
 let isNotificationAgreed = true;
 
 
+// 함수 
+// 1번
+function userWelcome(userName, userRank) {
+  const message = (userRank === 'VIP')
+    ? 'VIP ' + userName + '님, 특별한 혜택이 준비되어 있습니다!'
+    : '안녕하세요, ' + userName + '님! 즐거운 쇼핑 되세요.';
+  return message
+}
+console.log(userWelcome('사민재', 'D'));
+console.log(userWelcome('사민재', 'VIP'));
+
+
+// 2번
+const deliveryPrice = function (orderPrice, deliveryPlace) {
+  // 50000원 이상이면 0원, 아니면 3000원
+  const basicFee = (orderPrice >= 50000) ? 0 : 3000;
+  // 배송 지역이 제주거나 도서이면 배송비 3000원 추가 아니면 0원
+  const extraFee = ('제주' === deliveryPlace || '도서' === deliveryPlace) ? 3000 : 0;
+  // 주문 금액이; 0원 이하일 경우 0원 아니면 3000원
+  const result = (0 >= orderPrice) ? 0 : (basicFee + extraFee);
+
+  return result
+}
+
+console.log(deliveryPrice('제주') + '원' );
+console.log(deliveryPrice(20000, '서울') + '원' );
+console.log(deliveryPrice(70000, '제주') + '원' );
+console.log(deliveryPrice(20000, '도서') + '원');
+
+// 3번
+const isPasswordAvailable = (password) => password.length > 7 && password.length < 21
+
+console.log(isPasswordAvailable('qzwx1234'));
+console.log(isPasswordAvailable('qzwx1234567891234567'));
+console.log(isPasswordAvailable('qzwx12345678912345678'));
+console.log(isPasswordAvailable('qzwx123'));
+
+
+// 4번
+function pointAccrualCalculation(price, userLevel) {
+  const point
+      = (userLevel === 'VIP') ? price * 0.05
+      : (userLevel === 'GOLD') ? price * 0.03
+      : (userLevel === 'SILVER') ? price * 0.01
+      : price * 0.005;
+  return '적립금은 ' + parseInt(point) + '원 입니다.'
+}
+console.log(pointAccrualCalculation(50000, 'VIP'));
+console.log(pointAccrualCalculation(50000, 'GOLD'));
+console.log(pointAccrualCalculation(50000, 'SILVER'));
+console.log(pointAccrualCalculation(50000, ' '));
+
+// 5번
+const movieTicketPriceCalculation = function (type, isMorning, people) {
+  const normalMovie = 14_000
+  const specialMovie = 17_000
+  const imaxMovie = 20_000
+
+
+  const Fee
+      = (type === 'IMAX') ? imaxMovie
+      : (type === '3D') ? specialMovie
+      : normalMovie;
+
+  const movieValue = (isMorning === true) ? Fee * 0.8 : Fee 
+  // 할인율: 1 - 0.2 = 0.8
+  return movieValue * people +'원'
+
+}
+
+console.log(movieTicketPriceCalculation('3D', true, 2));
+console.log(movieTicketPriceCalculation('IMAX', false, 1));
+console.log(movieTicketPriceCalculation(' ', true, 3));
