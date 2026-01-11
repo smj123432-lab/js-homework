755333e9a6e30f93542407ce0f49597289e8ab9c
saminjae <smj123432@gmail.com>
2026-01-11 17:31:29 +0900

    과제: 변수 상수 선언

---
+++ homework/homework.js
@@ -0,0 +1,27 @@
+// 1. 영화 제목을 저장할 변수를 선언하고 좋아하는 영화 제목을 할당하세요.
+let favoritMovie = 'Jeon_Woo_chi';
+// 2. "빛의 속도(299,792,458 m/s)"를 담을 상수를 선언하세요.
+const lightSpeed = '299,792,458m/s';
+// 3. "이메일 인증 완료 여부"를 확인하는 불리언 타입 변수를 선언하고 적절한 값을 할당하세요.
+let isEmailAvailable = true;
+// 4. "상품 재고 수량(예: 47개)"을 담는 변수를 선언하고 숫자 값을 할당하세요.
+let productQuantity = '47개';
+// 5. "회원 포인트(예: 15,800점)"를 담는 변수를 작성하세요.
+let usetPoint = '15,800점';
+// 6. API 서버의 "기본 URL(예: https://api.example.com)을 담는 상수를 작성하고 값을 할당하세요.
+const APIserver = 'www.naver.com';
+// 7. 게시글의 "조회수(예: 1,234)"를 담는 변수를 선언하고 값을 할당하세요.
+let postTotalView = 1_247;
+// 8."배송 상태('준비중', '배송중', '배송완료' 중 하나)"를 담는 변수를 작성해보세요.
+let deliveryState = 'delivered';
+// 9. "쿠폰 사용 가능 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isCouponAvailable = true;
+// 10. "최대 업로드 파일 크기(예: 10MB를 바이트 단위로 10485760)"를 담는 상수를 작성하세요.
+// 10MB = 10 * 1024KB * 1024Bytes
+const MAX_FILE_SIZE = 10 * 1024 * 1024;
+// 11. "사용자 등급 점수(예: 85.5점)"를 담는 변수를 선언하고 소수점이 포함된 값을 할당하세요.
+let userRankPoint = 85.5;
+// 12. "알림 수신 동의 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isNotificationAgreed = true;
+
+
